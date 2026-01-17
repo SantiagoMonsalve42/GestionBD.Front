8f9a9927077de128c378c936f213428b6fa35aaa
Andrés Santiago Monsalve Salinas <Asmonsalves@intergrupo.com>
2026-01-17 11:32:46 -0500

    control dinamico de rutas por rol

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -21,45 +21,67 @@ export class NavbarComponent {
   constructor() {
     this.initMenuItems();
   }
+  username(): string|undefined{
+    return this.sessionService.getUsername();
+  }
 
   private initMenuItems(): void {
-    this.items = [
-      {
-        label: 'Proyectos',
-        icon: 'pi pi-folder',
-        items: [
-          {
-            label: 'Mis ejecuciones',
-            icon: 'pi pi-list',
-            command: () => this.router.navigate(['/core/executions'])
-          },
-          {
-            label: 'Revisar ejecuciones',
-            icon: 'pi pi-check',
-            command: () => this.router.navigate(['/core/connections'])
-          }
-        ]
-      },
-      {
-        label: 'Configuración',
-        icon: 'pi pi-cog',
-        items: [
-          {
-            label: 'Instancias',
-            icon: 'pi pi-server',
-            command: () => this.router.navigate(['/core/connections'])
-          }
-        ]
-      },
-      
-      {
-        label: 'Cerrar Sesión',
-        icon: 'pi pi-sign-out',
-        command: async () => {
-          await this.sessionService.logout();
-        }
-      },
-    ];
+    if(this.sessionService.checkIsAdmin()){
+      this.items = [
+                    {
+                      label: 'Proyectos',
+                      icon: 'pi pi-folder',
+                      items: [
+                        {
+                          label: 'Revisar ejecuciones',
+                          icon: 'pi pi-check',
+                          command: () => this.router.navigate(['/core/connections'])
+                        }
+                      ]
+                    },
+                    {
+                      label: 'Configuración',
+                      icon: 'pi pi-cog',
+                      items: [
+                        {
+                          label: 'Instancias',
+                          icon: 'pi pi-server',
+                          command: () => this.router.navigate(['/core/connections'])
+                        }
+                      ]
+                    },
+                    
+                    {
+                      label: 'Cerrar Sesión',
+                      icon: 'pi pi-sign-out',
+                      command: async () => {
+                        await this.sessionService.logout();
+                      }
+                    },
+                  ];
+    }else{
+      this.items = [
+                    {
+                      label: 'Proyectos',
+                      icon: 'pi pi-folder',
+                      items: [
+                        {
+                          label: 'Ejecuciones',
+                          icon: 'pi pi-list',
+                          command: () => this.router.navigate(['/core/executions'])
+                        }
+                      ]
+                    },
+                    {
+                      label: 'Cerrar Sesión',
+                      icon: 'pi pi-sign-out',
+                      command: async () => {
+                        await this.sessionService.logout();
+                      }
+                    },
+                  ];
+    }
+    
   }
   redirectHome(): void{
     this.router.navigate(['/core/dashboard']);
